6d013e7f00ed0d4f2a6b8f8c5309bd2858ca7bc8
Riccardo Foffi <riccardo.foffi@protonmail.com>
2021-02-05 20:58:00 +0100

    add IOUtils dependency; add gromacs parsing utilites

--- src/Particles.jl
+++ src/Particles.jl
@@ -1,10 +1,12 @@
 module Particles
 
 using LinearAlgebra
+using IOUtils
 using Reexport
 @reexport using StaticArrays
 
 include("types.jl")
 include("distances.jl")
+include("gromacs.jl")
 
 end # module
