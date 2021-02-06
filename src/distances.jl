bd1637cd63a472eebf589c8314bc58cc30f4d48a
Riccardo Foffi <riccardo.foffi@protonmail.com>
2021-02-06 15:57:18 +0100

    distancevector supports AbstractVector objects

--- src/distances.jl
+++ src/distances.jl
@@ -2,6 +2,16 @@ export
     distancevector,
     distance
 
+function distancevector(p1::AbstractVector, p2::AbstractVector)
+    return p2 .- p1
+end # function
+
+function distancevector(p1::AbstractVector, p2::AbstractVector, box)
+    δr = (p2 .- p1) ./ box
+    δr = (δr .- round.(δr)) .* box
+    return δr
+end # function
+
 function distancevector(p1::AbstractParticle, p2::AbstractParticle)
     return p2.r .- p1.r
 end # function
