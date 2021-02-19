8a1618b9a404acc76be363fb4ba9ba33075a37e2
Riccardo Foffi <riccardo.foffi@protonmail.com>
2021-02-19 23:33:25 +0100

    Update types.jl with moving particle types.

--- src/types.jl
+++ src/types.jl
@@ -1,9 +1,18 @@
 export
     AbstractParticle,
+    AbstractMParticle,
     Particle,
+    MParticle,
+    MVParticle,
     Atom
 
 abstract type AbstractParticle{D,T} end
+
+abstract type AbstractMParticle{D,T} <: AbstractParticle{D,T} end
+
+function Base.show(io::IO, p::T) where T<:AbstractParticle
+    print(io, "$(T) $(p.type) (r = $(p.r))")
+end # function
 
 struct Particle{D,T} <: AbstractParticle{D,T}
     type::String #identifier
@@ -13,11 +22,6 @@ end # struct
 function Particle(; type = "", r = zeros(SVector{3}))
     return Particle(type, r)
 end # function
-
-function Base.show(io::IO, p::AbstractParticle{D,T}) where D where T
-    print(io, "Particle{$(D),$(T)} $(p.type) (r = $(p.r))")
-end # function
-
 
 struct Atom <: AbstractParticle{3,Float64}
     type::String
@@ -47,3 +51,26 @@ end # function
 function Base.show(io::IO, a::Atom)
     print(io, "Atom $(a.type) (r = $(a.r); v = $(a.v))")
 end # function
+
+
+struct MParticle{D,T} <: AbstractMParticle{D,T}
+    type::String
+    r::MVector{D,T}
+end # struct
+
+function MParticle(; type = "", r = zeros(MVector{3}))
+    return MParticle(type, r)
+end # function
+
+
+struct MVParticle{D,T} <: AbstractMParticle{D,T}
+    type::String
+    r::MVector{D,T}
+    v::MVector{D,T}
+end # struct
+
+function MVParticle(; type = "", r = zeros(MVector{3}),
+                    v = zeros(MVector{3}))
+    return MVParticle(type, r, v)
+end # function
+
